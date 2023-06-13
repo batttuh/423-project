e8d77494bef20ae75e19b26799805dcad1bea3df
Ali Günay <ali.gunay001@gmail.com>
2023-06-13 11:31:42 +0300

    GetUserPostByID

--- Controllers/Authentication.cs
+++ Controllers/Authentication.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
+using System;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
@@ -14,15 +15,16 @@ namespace YourNamespace.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IUserRepository _userRepository;
-         private readonly IConfiguration _configuration;
-         private readonly IUserTypeRepository _userTypeRepository;
+        private readonly IPostRepository _postRepository;
+        private readonly IConfiguration _configuration;
+        private readonly IUserTypeRepository _userTypeRepository;
 
-        public AuthController(IUserRepository userRepository,IConfiguration configuration,IUserTypeRepository userTypeRepository)
+        public AuthController(IUserRepository userRepository, IConfiguration configuration, IPostRepository postRepository, IUserTypeRepository userTypeRepository)
         {
             _userRepository = userRepository;
             _configuration = configuration;
+            _postRepository = postRepository;
             _userTypeRepository = userTypeRepository;
-
         }
 
         [HttpPost("register")]
@@ -30,73 +32,81 @@ namespace YourNamespace.Controllers
         {
             Console.WriteLine("Registering user");
             Console.WriteLine("Registering user");
-            
-              if (!ModelState.IsValid)
-        {
-            return BadRequest(ModelState);
-        }
 
-        // Check if the username is already taken
-        if (_userRepository.GetUserByEmail(request.e_mail) != null)
-        {
-            ModelState.AddModelError("Username", "Username is already taken.");
-            return BadRequest(ModelState);
-        }
-        UserType userType=await _userTypeRepository.GetUserTypeById(request.UserTypeID);
-        var user = new User
-                {
-                    UserTypeID = request.UserTypeID,
-                    Name = request.Name,
-                    Password = request.Password,
-                    e_mail = request.e_mail,
-                    NameSurname = request.NameSurname,
-                    TiktokAccount = request.TiktokAccount,
-                    InstagramAccount = request.InstagramAccount,
-                    TiktokFollowerCount = request.TiktokFollowerCount,
-                    InstagramFollowerCount = request.InstagramFollowerCount,
-                    UserType = userType
-                };
-        
-        // Save the user to the database
-        _userRepository.CreateUser(user);
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
 
-        return Ok();
-           
+            // Check if the username is already taken
+            if (_userRepository.GetUserByEmail(request.e_mail) != null)
+            {
+                ModelState.AddModelError("Username", "Username is already taken.");
+                return BadRequest(ModelState);
+            }
+            Post post = await _postRepository.GetPostById(request.PostID);
+            UserType userType = await _userTypeRepository.GetUserTypeById(request.UserTypeID);
+            var user = new User
+            {
+                UserTypeID = request.UserTypeID,
+                Name = request.Name,
+                Password = request.Password,
+                PostID = request.PostID,
+                e_mail = request.e_mail,
+                NameSurname = request.NameSurname,
+                TiktokAccount = request.TiktokAccount,
+                InstagramAccount = request.InstagramAccount,
+                TiktokFollowerCount = request.TiktokFollowerCount,
+                InstagramFollowerCount = request.InstagramFollowerCount,
+                // Post ve UserType özellikleri için ilgili nesneleri de ayarlayabilirsiniz.
+                Post = post,
+                UserType = userType
+            };
+
+            // Save the user to the database
+            _userRepository.CreateUser(user);
+
+            return Ok();
         }
 
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserLogin request)
         {
-
+            Console.WriteLine(request.e_mail);
+            Console.WriteLine(request.Password);
             if (!ModelState.IsValid)
-        {
-            return BadRequest(ModelState);
-        }
+            {
+                return BadRequest(ModelState);
+            }
 
-        // Check if the user exists
-        var user = _userRepository.GetUserByEmail(request.e_mail);
-        if (user == null)
-        {
-            ModelState.AddModelError("Username", "Invalid username or password.");
-            return BadRequest(ModelState);
-        }
+            // Check if the user exists
+            var user = _userRepository.GetUserByEmail(request.e_mail);
+            if (user == null)
+            {
+                ModelState.AddModelError("Username", "Invalid username or password.");
+                return BadRequest(ModelState);
+            }
 
-        // Validate the password
-        if (!_userRepository.VerifyPassword(user,request.Password,user.Password))
-        {
-            ModelState.AddModelError("Username", "Invalid username or password.");
-            return BadRequest(ModelState);
-        }
+            // Validate the password
+            if (!_userRepository.VerifyPassword(user, request.Password, user.Password))
+            {
+                ModelState.AddModelError("Username", "Invalid username or password.");
+                return BadRequest(ModelState);
+            }
 
-        // TODO: Generate and return an authentication token (e.g., JWT) for the logged-in 
-        var token = GenerateJwtToken(user);
+            // Retrieve the user's posts
+            var posts = await _postRepository.GetPostsByUserId(user.UserID);
 
-         return Ok(new { Token = token });
+            // Generate JWT token
+            var token = GenerateJwtToken(user);
+
+            // Return the user and posts along with the token
+            return Ok(new { Token = token, User = user, Posts = posts });
         }
 
         private string GenerateJwtToken(User user)
         {
-             var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenHandler = new JwtSecurityTokenHandler();
             var key = Encoding.ASCII.GetBytes(_configuration["Jwt:SecretKey"]!);
             var tokenDescriptor = new SecurityTokenDescriptor
             {
--- Repository/IUserRepository.cs
+++ Repository/IUserRepository.cs
@@ -1,11 +1,17 @@
 using back_side_Model.Models;
+using System.Collections.Generic;
 
-public interface IUserRepository
+
+namespace back_side_DataAccess.Repositories
 {
-    User GetUserByEmail(string username);
-    void CreateUser(User user);
-    void UpdateUser(User user);
-    void DeleteUser(int userId);
-    List<User> GetAllUsers();
-    Boolean VerifyPassword(User user,string passwordRequest, string passwordUser);
+    public interface IUserRepository
+    {
+        User GetUserByEmail(string username);
+        void CreateUser(User user);
+        void UpdateUser(User user);
+        void DeleteUser(int userId);
+        List<User> GetAllUsers();
+        Boolean VerifyPassword(User user, string passwordRequest, string passwordUser);
+        List<Post> GetPostsByUserId(int userId);
+    }
 }
